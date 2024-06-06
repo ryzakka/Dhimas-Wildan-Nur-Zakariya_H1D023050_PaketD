b96d1223529b8c3f21862a86817a4f0641407ae3
ryzakka <161613798+ryzakka@users.noreply.github.com>
2024-06-06 14:25:45 +0700

    Update KHS01.c

--- KHS01.c
+++ KHS01.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+
+//deklarasi struktur KHS
+typedef struct{
+int NIM;
+float IP;
+informasi* KHS;
+};
+
+//buat fungsi cetak KHS
+void printKHS(){
+cetak = a* moneter  
+}
+
+//buat main program
+int main(){
+  
+}
+
+//cetak KHS sebelum perubahan
+printf("Sebelum IP diperbarui:\n");
+printKHS();
+printf("\n");
+
+//ubah IP Mahasiswa
+
+//cetak KHS sebelum Perubahan
+printf("setelah IP diperbarui:\n");
+printKHS(&p);
+
+return 0;
+}
 
