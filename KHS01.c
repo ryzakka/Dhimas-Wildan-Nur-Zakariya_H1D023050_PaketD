3acf9fda3d6a35ef7d09de2324b16aa1098ad706
ryzakka <161613798+ryzakka@users.noreply.github.com>
2024-06-06 14:32:44 +0700

    Update KHS01.c

--- KHS01.c
+++ KHS01.c
@@ -23,7 +23,9 @@ printKHS();
 printf("\n");
 
 //ubah IP Mahasiswa
-
+m2uxkn3nb23msoei2exngqhsm,wqvsqcqw,e'mx2nhz
+  dj1msi2y3xn12,aw12
+  `ZM1UZEY4YREimuew2y3n3y2x bxzn3m
 //cetak KHS sebelum Perubahan
 printf("setelah IP diperbarui:\n");
 printKHS(&p);
